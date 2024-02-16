59b8effdcb22445031ed827360d165331f4a3326
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-16 09:34:41 +0100

    Declaring needed classes for the game (in panel)

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,10 @@
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.*;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -9,9 +14,46 @@ public class GamePanel extends JPanel implements ActionListener{
 		
 	}
 	
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkApple() {
+		
+	}
+	
+	public void checkCollisions() {
+		
+	}
+	
+	public void gameOver(Graphics g) {
+		
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter{
+		
+		@Override
+			public void keyPressed(KeyEvent e) {
+			
+		}
 		
 	}
 
