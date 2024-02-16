723a068b795bcad68e3bc9df960d25fa2a16a753
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-16 09:48:06 +0100

    Displaying grid to better visualize

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -14,7 +14,7 @@ public class GamePanel extends JPanel implements ActionListener{
 
 	static final int SCREEN_WIDTH = 600;
 	static final int SCREEN_HEIGHT = 600;
-	static final int UNIT_SIZE = 25;
+	static final int UNIT_SIZE = 20;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
 	static final int DELAY = 75;
 	final int x[] = new int[GAME_UNITS];
@@ -49,9 +49,15 @@ public class GamePanel extends JPanel implements ActionListener{
 		draw(g);
 	}
 	
-	public void draw(Graphics g) {
-		
-	}
+	// Just to better visualize the grid
+		public void draw(Graphics g) {
+			
+			for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
+				g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+				g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+			}
+			
+		}
 	
 	public void newApple() {
 		
