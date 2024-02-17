eb4656d1c89b0987ad471b94e68ad2bcaa4d2547
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-17 10:51:08 +0100

    Hiding the grid and changing speed

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -18,7 +18,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	static final int SCREEN_HEIGHT = 600;
 	static final int UNIT_SIZE = 20;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-	static final int DELAY = 75;
+	static final int DELAY = 79;
 	final int x[] = new int[GAME_UNITS];
 	final int y[] = new int[GAME_UNITS];
 	int bodyParts = 6;
@@ -55,10 +55,10 @@ public class GamePanel extends JPanel implements ActionListener{
 		public void draw(Graphics g) {
 			
 			if(running) {
-				for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
-					g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-					g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-				}
+//				for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
+//					g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+//					g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+//				}
 				
 				g.setColor(Color.red);
 				g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
